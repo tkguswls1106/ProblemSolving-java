68b84aea957828cc040e995d5bf09e5908816c7a
sahyunjin <tkguswls1106@naver.com>
2024-10-04 04:11:41 +0900

    Add: Permutation_Basic

--- Problems_java/Permutation_Basic.java
+++ Problems_java/Permutation_Basic.java
@@ -1,33 +1,38 @@
 import java.util.*;
 
-// [ 조합(Combination) - 기초 정리 ]
-// 조합이란 n개의 원소 중에서 r개를 뽑는 경우의 수를 의미한다.
-// 이는 '순열처럼 순서가 바뀌어도 경우의수로 인정해주는것'과는 다르게 순서가 바뀌면 중복처리 되어 인정안된다.
+// [ 순열(Permutation) - 기초 정리 ]
+// 순열이란 서로 다른 n개의 원소에서 r개를 뽑아 한 줄로 세우는 부분수열 경우의 수를 의미한다.
+// 이는 '조합처럼 순서가 바뀌면 경우의수로 인정안해주는것'과는 다르게 순서가 바뀌어도 인정된다.
 
-public class Combination_Basic {
+public class Permutation_Basic {
     public static int n, m;  // 전체 개수, 선택 개수
-    public static int[] arr;  // 전체 요소 배열
+    public static int[] arr, visited;  // 전체 요소 배열, 방문여부 배열
     public static LinkedList<Integer> selected = new LinkedList<>();  // 선택 요소 리스트
     public static int answer = 0;  // 가능한 경우의 수
 
-    public static void comb(int start, int cnt) {
+    public static void perm(int cnt) {
         if(cnt == m) {
             System.out.println(selected.toString());
             answer++;
             return;
         }
 
-        for(int i=start; i<n; i++) {
+        for(int i=0; i<n; i++) {
+            if(visited[i] == 1) continue;
+            
             selected.add(arr[i]);
-            comb(i+1, cnt+1);  // comb(start+1, cnt+1); 로 작성하면 틀림.
+            visited[i] = 1;
+            perm(cnt+1);
             
             selected.removeLast();
+            visited[i] = 0;
         }
     }
 
     public static void main(String[] args) {
-        n = 4; m = 2;  // 4C2
+        n = 4; m = 2;  // 4P2
         arr = new int[n];
+        visited = new int[n];  // 자동으로 0으로 초기화.
 
         // 입력
         int[] inputArr = {1,7,8,9};  // 정렬되어있지않아도 상관없음.
@@ -36,7 +41,7 @@ public class Combination_Basic {
             arr[i] = inputNum;
         }
 
-        comb(0, 0);
+        perm(0);
         System.out.printf("==> 가능한 경우의 수 : %d\n", answer);
     }
 }
