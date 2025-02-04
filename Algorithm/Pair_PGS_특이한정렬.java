0cdee9204f50fe532ecf81ff9a45633acc0a1cdd
sahyunjin <tkguswls1106@naver.com>
2025-02-04 18:05:52 +0900

    Chore: 객체비교 equals 및 hashCode 설명 보충

--- Algorithm/Pair_PGS_특이한정렬.java
+++ Algorithm/Pair_PGS_특이한정렬.java
@@ -2,9 +2,19 @@ import java.util.*;
 import java.util.stream.*;  // Collectors
 
 // [ Pair 클래스 구현 및 커스텀 정렬 ]
-// - 주의사항 : Pair 클래스 작성 시,
-// 만약 정렬이 필요없어 '@Override compareTo()'를 미구현한다면, 'implements Comparable<Pair>'도 함께 제거해야만 한다.
+
+// - 주의사항 :
+// Pair 클래스 작성 시, 만약 정렬이 필요없어 '@Override compareTo()'를 미구현한다면, 'implements Comparable<Pair>'도 함께 제거해야만 함.
 // 참고로 이 문제의 경우, 사실 equals()와 hashCode() 메소드의 오버라이딩은 필요없음. 단지 사용법을 명세하여 익히고자 표기하였음.
+
+// - Set,Map 컬렉션에서 객체의 중복검사를 위해, equals()와 함께 hashCode()도 재정의해야 하는 이유 :
+// hashCode() 메소드는 객체의 필드'값'을 기반으로 해시코드를 부여하기에, 값이 모두 같다면 동일한 해시코드가 부여됨.
+// 객체들의 비교 과정으로, 서로의 hashCode() 반환값이 일치한 이후 equals() 반환값이 참일때만 같은 객체라고 판단함.
+// 따라서 equals() 재정의로 필드값 비교에 성공해도, 막상 hashCode()도 함께 재정의하지 않으면, 컬렉션에서 중복검사에 걸리지않아 또다시 중복 add 될 수 있음.
+
+// - equals() 및 hashCode() 특이사항 :
+// 만약 Set,Map 등 컬렉션에서의 중복검사 용도가 아닌, 단순히 두 객체의 필드값 비교를 목적으로 한다면,
+// hashCode() 없이 equals()만 재정의해도 'pairObj1.equals(pairObj2) == true'처럼 성공적인 '값'비교가 가능함.
 
 class Solution {
     public static class Pair implements Comparable<Pair> {  // !!! 주의사항: 주의사항을 위에 기재해두었음. !!!
@@ -44,7 +54,7 @@ class Solution {
         // - hashCode 오버라이딩 경우: HashSet, HashMap (!!! 사실 이 문제에선 필요 X !!!)
         @Override
         public int hashCode() {
-            return Objects.hash(num, dist);  // 해시 코드 생성 (HashSet,HashMap 등의 key 중복방지.)
+            return Objects.hash(num, dist);  // 해시 코드 생성 (필드'값'을 기준으로 부여. HashSet,HashMap 등의 key 중복방지.)
         }
         
         // - compareTo 오버라이딩 경우: 커스텀 정렬, TreeSet, TreeMap
