fb7bd87d2147157ab6651eea9db1c8c96622391e
sahyunjin <tkguswls1106@naver.com>
2025-02-08 04:34:57 +0900

    Fix: BOJ_1920 이분탐색 풀이 추가

--- PS/BOJ_1920.java
+++ PS/BOJ_1920.java
@@ -1,3 +1,6 @@
+// < 성공 방법 1 >
+// - 알고리즘: Set (이 문제의 경우, 이분탐색보다 Set 방식이 더 빠름.)
+
 import java.util.*;
 import java.io.*;
 
@@ -22,3 +25,36 @@ public class BOJ_1920 {
         System.out.print(stb.toString());
     }
 }
+
+/*
+// < 성공 방법 2 >
+// - 알고리즘: 이분 탐색
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stb = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer stt = new StringTokenizer(br.readLine());
+        while(n-- > 0) {
+            list.add(Integer.parseInt(stt.nextToken()));
+        }
+        Collections.sort(list);
+
+        int m = Integer.parseInt(br.readLine());
+        stt = new StringTokenizer(br.readLine());
+        while(m-- > 0) {
+            int findNum = Integer.parseInt(stt.nextToken());
+            boolean isExist = (Collections.binarySearch(list, findNum) >= 0) ? true : false;
+            stb.append(isExist ? 1 : 0).append("\n");
+        }
+
+        System.out.print(stb.toString());
+    }
+}
+*/
