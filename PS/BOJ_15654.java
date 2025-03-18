23186858eb978748dac11911beab50d79be6be84
sahyunjin <tkguswls1106@naver.com>
2025-03-19 02:34:35 +0900

    Solve: BOJ_15654

--- PS/BOJ_15654.java
+++ PS/BOJ_15654.java
@@ -1,16 +1,16 @@
 import java.util.*;
 import java.io.*;
 
-// - 알고리즘: 조합
+// - 알고리즘: 순열
 // - 문제 Tip: 문제에 '주어지는 N개의 자연수는 모두 다른 수이다'라고 명시되어있으므로, 입력 요소들의 중복제거는 하지않아도됨.
 
-public class BOJ_15655 {
+public class BOJ_15654 {
     public static int n, m;
-    public static int[] arr;
+    public static int[] arr, visited;
     public static List<Integer> selected = new ArrayList<>();
     public static StringBuilder stb = new StringBuilder();
 
-    public static void comb(int start, int cnt) {
+    public static void perm(int cnt) {
         if(cnt == m) {
             for(int num : selected) {
                 stb.append(num).append(" ");
@@ -19,11 +19,15 @@ public class BOJ_15655 {
             return;
         }
 
-        for(int i=start; i<n; i++) {
+        for(int i=0; i<n; i++) {
+            if(visited[i] == 1) continue;
+
             selected.add(arr[i]);
-            comb(i+1, cnt+1);
+            visited[i] = 1;
+            perm(cnt+1);
 
             selected.remove(selected.size()-1);
+            visited[i] = 0;
         }
     }
 
@@ -33,6 +37,7 @@ public class BOJ_15655 {
         n = Integer.parseInt(stt.nextToken());
         m = Integer.parseInt(stt.nextToken());
         arr = new int[n];
+        visited = new int[n];
 
         stt = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
@@ -40,7 +45,7 @@ public class BOJ_15655 {
         }
         Arrays.sort(arr);  // 문제에 '수열은 사전 순으로 증가하는 순서로 출력'라고 명시되어있기 때문.
 
-        comb(0, 0);
+        perm(0);
 
         System.out.print(stb.toString());
     }
