c3f9d0e697acfc20751da22de1b9559d0fc5e150
sahyunjin <tkguswls1106@naver.com>
2025-04-04 20:57:58 +0900

    Fix: DP_BOJ_2579 성능개선 풀이 추가

--- Algorithm/DP_BOJ_2579.java
+++ Algorithm/DP_BOJ_2579.java
@@ -1,23 +1,99 @@
+// < 성공 방법 1 >
+// dp[] 1차원 배열로 푸는 방법.
+
 import java.util.*;
 import java.io.*;
 
 // [ DP 계단 문제 ]
 
-// - 문제 Tip:
+// - dp[] 1차원 풀이방식 Tip :
+// arr[i] = 포함된것이 확정일때 (위치 O)
+// dp[i] = arr[i]를 포함하든안하든 i위치까지 도달했을때의 최댓값 (위치 OorX)
+// 1. OorX X  // i계단을 밟지않는 경우 => i-1계단은 모두 가능하므로 dp[i-1] 사용.
+// dp1 = dp[i-1] + (i 포함X)
+//     = dp[i-1]
+// 2. OorX X O  // i-1계단을 밟지않고, i계단을 밟는 경우 => i-2계단은 모두 가능하므로 dp[i-2] 사용.
+// dp2 = dp[i-2] + (i-1 포함X) + arr[i]
+//     = dp[i-2] + arr[i]
+// 3. OorX X O O  // i계단과 i-1계단을 밟고, i-2계단을 밟지않는 경우 => i-3계단은 모두 가능하므로 dp[i-3] 사용.
+// dp3 = dp[i-3] + (i-2 포함X) + arr[i-1] + arr[i]
+//     = dp[i-3] + arr[i-1] + arr[i]
+// ==> dp[i] = max(dp1, dp2, dp3)
+// 참고로 이 문제는 마지막 계단을 반드시 포함하므로, dp1 없이 'max(dp2, dp3)'로 적용할것.
+
+// - {번외} dp[] 1차원 풀이방식 Tip (설명만 다름) :
+// 1. 마지막 계단에서 한칸 내려가는 경우:
+// step1 = stair[i] + (stair[i - 1] + dp[i - 3])
+// 2. 마지막 계단에서 두칸을 내려가는 경우:
+// step2 = stair[i] + dp[i - 2]
+// ==> dp[i] = max(step1, step2)
+
+public class DP_BOJ_2579 {
+    // - dp 테이블 정의 :
+    public static int[] dp = new int[302];  // stair[i]를 포함하든안하든 i위치까지 도달했을때의 최댓값 (위치 OorX)
+    public static int[] stair = new int[302];  // 포함된것이 확정일때 (위치 O)
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=n; i++) {
+            stair[i] = Integer.parseInt(br.readLine());
+        }
+
+        // - 점화식 찾기 :
+        // 1. OorX X 경우
+        // dp1 = dp[i-1] + (i 포함X)
+        //     = dp[i-1]
+        // 2. OorX X O 경우
+        // dp2 = dp[i-2] + (i-1 포함X) + stair[i]
+        //     = dp[i-2] + stair[i]
+        // 3. OorX X O O 경우
+        // dp3 = dp[i-3] + (i-2 포함X) + stair[i-1] + stair[i]
+        //     = dp[i-3] + stair[i-1] + stair[i]
+        // ==> dp[i] = max(dp1, dp2, dp3)
+        // 참고로 이 문제는 마지막 계단을 반드시 포함하므로, dp1 없이 'max(dp2, dp3)'로 적용할것.
+
+        // - 초기값 할당 :
+        dp[1] = stair[1];
+        if(2 <= n) {
+            dp[2] = stair[1] + stair[2];
+        }
+        if(3 <= n) {
+            // int dp1 = dp[3-1];
+            int dp2 = dp[3-2] + stair[3];
+            int dp3 = stair[3-1] + stair[3];  // dp[3-3] + stair[3-1] + stair[3]
+            dp[3] = Math.max(dp2, dp3);
+        }
+
+        // - dp값 세팅 :
+        for(int i=4; i<=n; i++) {
+            // int dp1 = dp[i-1];
+            int dp2 = dp[i-2] + stair[i];
+            int dp3 = dp[i-3] + stair[i-1] + stair[i];
+
+            dp[i] = Math.max(dp2, dp3);
+        }
+
+        int answer = dp[n];
+        System.out.print(answer);
+    }
+}
+
+/*
+// < 성공 방법 2 >
+// dp[][] 2차원 배열로 푸는 방법.
+
+import java.util.*;
+import java.io.*;
+
+// - dp[][] 2차원 풀이방식 Tip :
 // 1. 현재포함 연속 한계단 오른 경우 dp[i][1]
 // => 이전계단은 연속이 끊겼던 상태임 dp[i-2][]
 // => i-2 계단까지 '한번연속 dp[i-2][1]'과 '두번연속 dp[i-2][2]' 모두 가능.
 // 2. 현재포함 연속 두계단 오른 경우 dp[i][2]
 // => 바로 이전계단을 반드시 밟았다는 의미임 dp[i-1][]
 // => i-1 계단까지는 두번연속은 안되고 '한번연속 dp[i-1][1]'만 가능.
-
-// - 번외(다른 점화식 도출 풀이방식):
-// 참고로 난 이 방식으로 풀진 않았음.
-// 1. 마지막 계단에서 한칸 내려가는 경우:
-// step1 = stair[i] + (stair[i - 1] + dp[i - 3])
-// 2. 마지막 계단에서 두칸을 내려가는 경우:
-// step2 = stair[i] + dp[i - 2]
-// ==> dp[i] = max(step1, step2)
 
 public class DP_BOJ_2579 {
     // - dp 테이블 정의 :
@@ -71,3 +147,4 @@ public class DP_BOJ_2579 {
         System.out.print(answer);
     }
 }
+*/
