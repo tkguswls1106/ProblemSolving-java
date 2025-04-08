eea816c4d1e6191ff22f822ff88d252417f86e19
sahyunjin <tkguswls1106@naver.com>
2025-04-08 23:33:57 +0900

    Solve: BOJ_11052

---
+++ PS/BOJ_11052.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+// - 알고리즘 : DP
+
+public class BOJ_11052 {
+    public static int[] arr = new int[10002];
+    public static int[] dp = new int[10002];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer stt = new StringTokenizer(br.readLine());
+        for(int i=1; i<=n; i++) {
+            arr[i] = Integer.parseInt(stt.nextToken());
+        }
+
+        // - 점화식 찾기 :
+        // 총 k(i)개의 카드를 구매해야할때, 현재 구매할 카드가 cur개짜리 카드팩이라면?
+        // => dp[k] = arr[cur] + dp[k-cur];
+        
+        // - 초기값 할당 :
+        dp[1] = arr[1];
+
+        // - dp값 세팅 :
+        for(int i=1; i<=n; i++) {
+            for(int cur=1; cur<=i; cur++) {
+                int curPrice = arr[cur] + dp[i-cur];
+                dp[i] = Math.max(dp[i], curPrice);
+            }
+        }
+
+        System.out.print(dp[n]);
+    }
+}
