5b2e3ac6ca92d83d3fd73b02e7b653d90be7861f
sahyunjin <tkguswls1106@naver.com>
2025-04-15 01:27:41 +0900

    Fix: PGS_전력망을둘로나누기 인접행렬 풀이 추가

--- PS/PGS_전력망을둘로나누기.java
+++ PS/PGS_전력망을둘로나누기.java
@@ -1,3 +1,10 @@
+// < 성공 방법 1 >
+// 'ArrayList<Integer>[]' 사용 방법
+// => 장점 1 : 메모리 사용이 효율적임.
+// => 장점 2 : 인접 노드의 탐색이 빠름.
+// => 단점 : 'adj[num1].remove(Integer.valueOf(num2))'에서 시간복잡도 O(간선개수)를 가지므로, 간선의 삭제가 느림.
+// ==> '노드 수 (n개)'가 작고 '간선 수가 최대 (n-1개)'인 트리 구조에서는, 'ArrayList<Integer>[]' 방식이 약간 더 효율적임.
+
 import java.util.*;
 
 // - 알고리즘: BFS + 브루트포스
@@ -75,6 +82,88 @@ class Solution {
         return result;
     }
 }
+
+/*
+// < 성공 방법 2 >
+// 'boolean[][]' 사용 방법
+// => 장점 : 'arr[num1][num2] = false'에서 시간복잡도 O(1)을 가지므로, 간선의 삭제가 빠름.
+// => 단점 1 : 2차원 배열이므로 메모리 사용이 비효율적임.
+// => 단점 2 : 'for(int nx=1; nx<=lastN; nx++)'에서 전체 노드에 매번 for문을 돌아야하므로, 인접 노드의 탐색이 느림.
+// ==> '노드 수 (n개)'가 작고 '간선 수가 최대 (n-1개)'인 트리 구조에서는, 'boolean[][]' 방식이 약간 더 비효율적임.
+
+import java.util.*;
+
+class Solution {
+    public static boolean[][] arr = new boolean[102][102];
+    public static boolean[] visited = new boolean[102];
+    public static int lastN = 0;
+    
+    public static int bfs(int x) {
+        if(visited[x] == true) return 0;
+        
+        Queue<Integer> qu = new LinkedList<>();
+        qu.offer(x);
+        visited[x] = true;
+        int nodeCnt = 1;
+        
+        while(!qu.isEmpty()) {
+            int cur = qu.poll();
+            
+            for(int nx=1; nx<=lastN; nx++) {
+                if(nx == cur) continue;
+
+                if(arr[cur][nx] == false || visited[nx] == true) continue;
+
+                qu.offer(nx);
+                visited[nx] = true;
+                nodeCnt++;
+            }
+        }
+        
+        return nodeCnt;
+    }
+    
+    public int solution(int n, int[][] wires) {
+        lastN = n;
+        
+        for(int[] wire : wires) {
+            int num1 = wire[0];
+            int num2 = wire[1];
+            arr[num1][num2] = true;
+            arr[num2][num1] = true;
+        }
+        
+        int result = Integer.MAX_VALUE;
+        for(int[] wire : wires) {
+            int num1 = wire[0];
+            int num2 = wire[1];
+            arr[num1][num2] = false;
+            arr[num2][num1] = false;
+            
+            List<Integer> cntList = new ArrayList<>();
+            for(int i=1; i<=n; i++) {
+                int cnt = bfs(i);
+                if(cnt != 0) {
+                    cntList.add(cnt);
+                    if(cntList.size() > 2) break;
+                }
+            }
+            
+            if(cntList.size() == 2) {
+                int diff = Math.abs(cntList.get(0) - cntList.get(1));
+                result = Math.min(result, diff);
+            }
+            
+            arr[num1][num2] = true;
+            arr[num2][num1] = true;
+            Arrays.fill(visited, false);
+            cntList.clear();
+        }
+        
+        return result;
+    }
+}
+*/
 
 public class PGS_전력망을둘로나누기 {
     public static void main(String[] args) {
