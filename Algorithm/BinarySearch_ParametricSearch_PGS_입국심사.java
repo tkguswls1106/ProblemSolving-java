add2e56174ac3f1a0234d614d593579b1d1f711d
sahyunjin <tkguswls1106@naver.com>
2025-05-25 18:30:08 +0900

    Fix: ParametricSearch 최소 도출법 변경

--- Algorithm/BinarySearch_ParametricSearch_PGS_입국심사.java
+++ Algorithm/BinarySearch_ParametricSearch_PGS_입국심사.java
@@ -4,6 +4,8 @@ import java.util.*;
 // - 코드 특징 :
 // 최소를 구해야하므로, 반대로 결정함수에서 minTimeSum이 아닌 maxTimeSum을 사용.
 // 또한 이러한 max 방식은 while문 조건과 범위를 좁히는 과정도 기존 min 방식과는 다름.
+// (참고로 이는 두 가지 방식이 있는데, 코드 내 기재한 '방식 1'을 더 권장함.
+// 왜냐하면 '방식 2'의 경우, right 초기값을 +1 해주고 풀어야하는 문제도 간혹 있기 때문.)
 
 class Solution {
     // 결정 함수 : 주어진 시간합계(maxTimeSum) 이하로 모두 심사할 수 있는가? (아직 시간합계을 더 줄일수 있는 확장성이 있는가?)
@@ -19,39 +21,44 @@ class Solution {
     
     public long solution(int n, int[] times) {
         Arrays.sort(times);
-        
+
+        // < 방식 1 >
         // left, right, mid 값 = 심사를 위해 주어지는 시간합계 범위를 의미.
         long left = 1;  // 'long left = times[0] * (long) n;' 사용시 틀림.
         long right = times[times.length-1] * (long) n;
-        while(left < right) {  // 'left <= right' 사용시 틀림.
+        long answer = 0;
+        while(left <= right) {
             long mid = (left + right) / 2;  // time '합계' 기준
-            
+        
             if(isValid(mid, n, times) == true) {
-                right = mid;  // 'left = mid + 1;' 사용시 틀림.
+                answer = mid;
+                right = mid - 1;  // 'left = mid + 1;' 사용시 틀림.
                 // 현재의 최대시간합계로 모두 심사가 가능하다면, 더 짧은 시간합계도 가능한지 확인.
             }
             else {
                 left = mid + 1;  // 'right = mid - 1;' 사용시 틀림.
             }
         }
-
-        return right;
         
-        // 또는 이 코드를 활용해도됨.
-        // int answer = 0;
-        // while(left <= right) {
-        //     long mid = (left + right) / 2;
+        return answer;
+        
+        // < 방식 2 >
+        // // left, right, mid 값 = 심사를 위해 주어지는 시간합계 범위를 의미.
+        // long left = 1;  // 'long left = times[0] * (long) n;' 사용시 틀림.
+        // long right = times[times.length-1] * (long) n;
+        // while(left < right) {  // 'left <= right' 사용시 틀림.
+        //     long mid = (left + right) / 2;  // time '합계' 기준
         //
         //     if(isValid(mid, n, times) == true) {
-        //         answer = mid;
-        //         right = mid - 1;
+        //         right = mid;  // 'left = mid + 1;' 사용시 틀림.
+        //         // 현재의 최대시간합계로 모두 심사가 가능하다면, 더 짧은 시간합계도 가능한지 확인.
         //     }
         //     else {
-        //         left = mid + 1;
+        //         left = mid + 1;  // 'right = mid - 1;' 사용시 틀림.
         //     }
         // }
         //
-        // return answer;
+        // return right;
     }
 }
 
