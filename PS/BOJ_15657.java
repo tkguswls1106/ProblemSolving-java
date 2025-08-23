9575af594a6f0805a5343a6dd7a4137185bd084b
sahyunjin <tkguswls1106@naver.com>
2025-08-23 19:25:08 +0900

    Fix: BOJ_15657 중복조합 풀이 추가

--- PS/BOJ_15657.java
+++ PS/BOJ_15657.java
@@ -1,8 +1,59 @@
+// < 성공 방법 1 >
+// - 알고리즘: 중복조합
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_15657 {
+    public static int n, m;
+    public static List<Integer> allList = new ArrayList<>();
+    public static List<Integer> selected = new ArrayList<>();
+    public static StringBuilder stb = new StringBuilder();
+
+    public static void comb(int start, int cnt) {
+        if(cnt == m) {
+            for(int num : selected) {
+                stb.append(num).append(" ");
+            }
+            stb.append("\n");
+            return;
+        }
+        
+        for(int i=start; i<n; i++) {
+            int num = allList.get(i);
+
+            selected.add(num);
+            comb(i, cnt+1);
+
+            selected.remove(selected.size()-1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stt = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(stt.nextToken());
+        m = Integer.parseInt(stt.nextToken());
+
+        stt = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            allList.add(Integer.parseInt(stt.nextToken()));
+        }
+        Collections.sort(allList);
+
+        comb(0, 0);
+
+        System.out.print(stb.toString());
+    }
+}
+
+/*
+// < 성공 방법 2 >
+// - 알고리즘: 중복순열 + 오름차순
+
 import java.util.*;
 import java.util.stream.*;
 import java.io.*;
-
-// - 알고리즘: 중복순열 
 
 public class BOJ_15657 {
     public static int n, m;
@@ -51,3 +102,4 @@ public class BOJ_15657 {
         System.out.print(stb.toString());
     }
 }
+*/
