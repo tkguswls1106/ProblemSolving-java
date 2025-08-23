d1594113d0588e56b2585bf3c01893fbb7e59f8b
sahyunjin <tkguswls1106@naver.com>
2025-08-23 12:29:33 +0900

    Fix: BOJ_15650 조합 풀이 추가

--- PS/BOJ_15650.java
+++ PS/BOJ_15650.java
@@ -1,6 +1,43 @@
+// < 성공 방법 1 >
+// - 알고리즘: 조합
+
 import java.util.*;
 
+public class BOJ_15650 {
+    public static int n, m;
+    public static List<Integer> selected = new ArrayList<>();
+    public static StringBuilder stb = new StringBuilder();
+
+    public static void comb(int start, int cnt) {
+        if(cnt == m) {
+            for(int num : selected) stb.append(num + " ");
+            stb.append("\n");
+            return;
+        }
+
+        for(int i=start; i<=n; i++) {  // 오름차순 되어있음 (그리고 오름차순 문제라 더더욱 순열이 아닌 조합인것.)
+            selected.add(i);
+            comb(i+1, cnt+1);
+
+            selected.remove(selected.size()-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        comb(1, 0);
+        System.out.print(stb.toString());
+    }
+}
+
+/*
+// < 성공 방법 2 >
 // - 알고리즘: 순열 + 오름차순
+
+import java.util.*;
 
 public class BOJ_15650 {
     public static int n, m;
@@ -37,3 +74,4 @@ public class BOJ_15650 {
         System.out.print(stb.toString());
     }
 }
+*/
